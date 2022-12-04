c8344f767314dd4b103132b309e266304f3a1d7e
nabil <nabil.nadzor@gmail.com>
2022-12-04 19:10:40 +0100

    Created Handler method to for Edit Post Request, created Thymeleaf tenplate for edit post, created handler method to update edited post data

--- src/main/java/com/example/springbootblogwebapp/service/PostService.java
+++ src/main/java/com/example/springbootblogwebapp/service/PostService.java
@@ -8,4 +8,8 @@ public interface PostService {
     List<PostDto> findAllPosts();
 
     void createPost(PostDto postDto);
+
+    PostDto findPostById(Long postId);
+
+    void updatePost(PostDto postDto);
 }
