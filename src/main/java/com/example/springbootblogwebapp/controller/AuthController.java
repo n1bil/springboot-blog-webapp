f5852fcbaec9e5583c7def509cefb266c7cd8d3f
nabil <nabil.nadzor@gmail.com>
2022-12-12 23:39:25 +0100

    Add spring security, default login and logout features. Created custom login form and configure spring security. Configured spring security for roles. Logout features implementation. Database authentication implementation

--- src/main/java/com/example/springbootblogwebapp/controller/AuthController.java
+++ src/main/java/com/example/springbootblogwebapp/controller/AuthController.java
@@ -20,6 +20,13 @@ public class AuthController {
         this.userService = userService;
     }
 
+    // handler method to handle login page request
+    @GetMapping("/login")
+    public String loginPage() {
+
+        return "login";
+    }
+
     // handler method to handle user registration request
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
