7f1b32e7391c2c54cee89b31e5ecf1899f387733
nabil <nabil.nadzor@gmail.com>
2022-12-25 11:41:30 +0100

    Added custom error page. Added specific error pages for 403, 404 and 500 error codes

--- src/main/java/com/example/springbootblogwebapp/controller/BlogController.java
+++ src/main/java/com/example/springbootblogwebapp/controller/BlogController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-public class BlogController {
+public class  BlogController {
 
     private PostService postService;
 
